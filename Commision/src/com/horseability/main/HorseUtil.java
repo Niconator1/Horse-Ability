8ea24c37f59ed4dc6014dd4fca11083a9e45262d
Niconator1 <1niconator1@gmail.com>
2016-07-28 17:42:46 +0200

    Added new classes

---
+++ Commision/src/com/horseability/main/HorseUtil.java
@@ -0,0 +1,13 @@
+package com.horseability.main;
+import org.bukkit.util.Vector;
+
+public class HorseUtil {
+	public static final Vector rotateAroundAxisY(Vector v, double angle) {
+		double x, z, cos, sin;
+		cos = Math.cos(angle);
+		sin = Math.sin(angle);
+		x = v.getX() * cos + v.getZ() * sin;
+		z = v.getX() * -sin + v.getZ() * cos;
+		return v.setX(x).setZ(z);
+	}
+}
